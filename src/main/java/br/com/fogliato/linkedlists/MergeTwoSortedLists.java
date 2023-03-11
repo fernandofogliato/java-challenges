6aa2cdba52952a53c9318252010043ea46408c5a
Fernando Fogliato <fernandofogliato@gmail.com>
2023-03-11 09:52:40 -0300

    Add more solutions

--- src/main/java/br/com/fogliato/linkedlists/MergeTwoSortedLists.java
+++ src/main/java/br/com/fogliato/linkedlists/MergeTwoSortedLists.java
@@ -20,17 +20,4 @@ public class MergeTwoSortedLists {
         cur.next = list1 == null ? list2 : list1;
         return sorted.next;
     }
-
-    public static void main(String[] args) {
-        ListNode list1 = new ListNode(1, new ListNode(2, new ListNode(4)));
-        ListNode list2 = new ListNode(1, new ListNode(3, new ListNode(4)));
-
-        ListNode result = mergeTwoLists(list1, list2);
-        System.out.print(result.val);
-        ListNode aux = result.next;
-        while (aux != null) {
-            System.out.print(aux.val);
-            aux = aux.next;
-        }
-    }
 }
